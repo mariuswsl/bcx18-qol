c7bd578820911321ebe34af5065d7c5f221cbe9b
Marius Wessel <github@mwessel.com>
2018-02-21 21:24:01 +0100

    test

--- cmxreceiver.js
+++ cmxreceiver.js
@@ -38,6 +38,7 @@ var ObjectID = mongodb.ObjectID;
 
 app.use(bodyParser({limit: '50mb'}));
 // app.use(bodyParser.json())
+app.use(bodyParser.text({ type: 'text/html' }))
 
 
 app.use(express.static(path.join(__dirname)));
