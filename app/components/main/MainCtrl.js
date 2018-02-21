59d0d8f5244234ffff86e7034b9604cae27da61f
Marius Wessel <github@mwessel.com>
2018-02-21 20:16:20 +0100

    test

--- app/components/main/MainCtrl.js
+++ app/components/main/MainCtrl.js
@@ -10,6 +10,14 @@ function MainCtrl($scope, $compile, $log, $uibModal, $rootScope, $window, MainSe
     MainService.restCall()
       .then(function (result) {
           $log.debug('result', result);
+
+          let data                   = result.data,
+              lastReceivedObject     = data.pop(),
+              lastReceivedObjectData = lastReceivedObject.data;
+              devicesCount           = lastReceivedDataobject.observations.length;
+
+          $scope.devicesCount = devicesCount;
+
         },
         function (error) {
           $log.debug("Failed rest call, with error:");
